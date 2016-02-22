9cef3d6765d05dda23eee1d25e4e57a76c36f0a2
James <jbkuczma@buffalo.edu>
2016-02-22 16:16:21 -0500

    starting to implement new way of retrieving NBA info

--- scores.py
+++ scores.py
@@ -82,55 +82,55 @@ def makeNHLDate(date): #nhl takes date in a different format. same code as check
         print("The date provided is not valid")
         exit(0)
 
-def getDate(date):
+def makeNBADate(date):
+    count = 0
     if checkDate(date):
-        return date
+        month = ""
+        day = ""
+        year = ""
+        for c in date:
+            if count == 0:
+                if c == '/':
+                    count+=1
+                else:
+                    month+=c
+            elif count == 1:
+                if c == '/':
+                    count+=1
+                else:
+                    day+=c
+            elif count == 2:
+                if c == '/':
+                    count+=1
+                else:
+                    year+=c
+        if int(day) >= 1 and int(day) <= 9:
+            day = str(0) + day
+        if int(month) >= 1 and int(month) <= 9:
+            month = str(0) + month
+        return year + month + day
     else:
         print("The date provided is not valid")
         exit(0)
-    # return time.strftime("%x") #returns current date in month/day/year format
+# def getDate(date):
+#     if checkDate(date):
+#         return date
+#     else:
+#         print("The date provided is not valid")
+#         exit(0)
+#     # return time.strftime("%x") #returns current date in month/day/year format
 
 def nbaScores(date,league):
-    maxTeamLength = 14 #Oklahoma City is the longest team city name
-    spacesNeeded = 0
     try:
-        count = 0
-        # url = 'http://sports.espn.go.com/%s/bottomline/scores' % league #probably not using this for NBA
-        ###
         headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0'}
-        url = "http://stats.nba.com/stats/scoreboard/?GameDate=%s&LeagueID=00&DayOffset=0" % getDate(date)
-        response = requests.get(url, headers=headers)
+        # url = 'http://sports.espn.go.com/%s/bottomline/scores' % league
+        url = 'http://espn.go.com/nba/scoreboard/_/date/%s' % makeNBADate(date) #date must be yyyymmdd
+        response = requests.get(url=url, headers=headers)
         response.raise_for_status()
-        data = response.json()['resultSets'][1]['rowSet']
-        for i in data:
-            pp = pprint.PrettyPrinter(indent=4)
-            team = i[5]
-            if not i[7]:
-                q1Score = 0
-            else:
-                q1Score = i[7]
-            if not i[8]:
-                q2Score = 0
-            else:
-                q2Score = i[8]
-            if not i[9]:
-                q3Score = 0
-            else:
-                q3Score = i[9]
-            if not i[10]:
-                q4Score = 0
-            else:
-                q4Score = i[10]
-            if not i[21]:
-                finalScore = 0
-            else:
-                finalScore = i[21]
-            if len(team) < maxTeamLength:
-                spacesNeeded = maxTeamLength - len(team)
-            pp.pprint(team + ":" + " " * spacesNeeded + str(q1Score) +"|" + str(q2Score) + "|" + str(q3Score) + "|" + str(q4Score) + " => " + str(finalScore)) #testing
-            count+=1
-            if count % 2 == 0:
-                print(colors.bcolors.OKGREEN + "===================================" + colors.bcolors.ENDC)
+        # use bs4 to parse
+        # data = response.text
+        # data = json.loads(data)
+        print(data)
     except Exception as e:
         print(e)
 
