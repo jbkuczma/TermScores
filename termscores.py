da157002da529fd192aa889d033d0704da6e8ce5
James <jbkuczma@buffalo.edu>
2016-03-04 15:39:04 -0500

    removed date flag. this way seems easier

--- termscores.py
+++ termscores.py
@@ -1,4 +1,3 @@
-import sys
 import argparse
 import scores
 import datetime as dt
@@ -8,17 +7,16 @@ def main():
     dateStr = "{}/{}/{}".format(date.month,date.day,date.year)
     parser = argparse.ArgumentParser("See live scores in your terminal")
     parser.add_argument("-nfl", action='store_true', help="NFL flag") #action='store_true' allows flag without argument
-    parser.add_argument("-nhl", action='store_true', help="Get game scores for NHL games")
-    parser.add_argument("-nba", action='store_true', help="Get game scores for NBA games") #argument must be in mm/dd/yyyy format
-    parser.add_argument("-nbaQ", action='store_true', help="Get game scores per quarter for NBA games")
-    parser.add_argument("-d", nargs='?', const=dateStr, help="Date flag (default: TODAY)")
+    parser.add_argument("-nhl", nargs='?', const=dateStr, help="Get game scores for NHL games on provided date (default: TODAY)")
+    parser.add_argument("-nba", nargs='?', const=dateStr, help="Get game scores for NBA games on provided date (default: TODAY)") #argument must be in mm/dd/yyyy format
+    parser.add_argument("-nbaQ", nargs='?', const=dateStr, help="Get game scores per quarter for NBA games on provided date (default: TODAY)")
     args = parser.parse_args()
     if args.nba:
-        scores.nbaScores(args.d)
+        scores.nbaScores(args.nba)
     elif args.nbaQ:
-        scores.nbaQuarterScores(args.d)
+        scores.nbaQuarterScores(args.nbaQ)
     elif args.nhl:
-        scores.nhlScores(args.d)
+        scores.nhlScores(args.nhl)
     elif args.nfl:
         print("NFL score feature coming soon")
     else:
