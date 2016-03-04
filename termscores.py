8767e6f56f69b9d53a0748e4e3335dc6acf231c1
James <jbkuczma@buffalo.edu>
2016-03-04 15:28:13 -0500

    added date flag

--- termscores.py
+++ termscores.py
@@ -1,28 +1,28 @@
 import sys
 import argparse
 import scores
+import datetime as dt
 
 def main():
+    date = dt.datetime.now()
+    dateStr = "{}/{}/{}".format(date.month,date.day,date.year)
     parser = argparse.ArgumentParser("See live scores in your terminal")
     parser.add_argument("-nfl", action='store_true', help="NFL flag") #action='store_true' allows flag without argument
-    parser.add_argument("-nhl", help="Get game scores for NHL games on provided date")
-    parser.add_argument("-nba", help="Get game scores for NBA games on provided date") #argument must be in mm/dd/yyyy format
-    parser.add_argument("-nbaQ", help="Get game scores per quarter for NBA games on provided date")
-    # note: maybe separate the date argument from the league flag, make date its own flag
-    # |-> ex: -nba -d 3/3/2016 ... if -d isn't provided then default to today's date
+    parser.add_argument("-nhl", action='store_true', help="Get game scores for NHL games")
+    parser.add_argument("-nba", action='store_true', help="Get game scores for NBA games") #argument must be in mm/dd/yyyy format
+    parser.add_argument("-nbaQ", action='store_true', help="Get game scores per quarter for NBA games")
+    parser.add_argument("-d", nargs='?', const=dateStr, help="Date flag (default: TODAY)")
     args = parser.parse_args()
-    if len(sys.argv) > 3:
-        parser.print_help()
-    elif args.nfl:
-        # http://www.nfl.com/liveupdate/scorestrip/ss.xml
-        # http://www.nfl.com/liveupdate/scorestrip/postseason/ss.xml
-        print("You want scores for the NFL")
-    elif args.nhl:
-        scores.nhlScores(args.nhl)
-    elif args.nba:
-        scores.nbaScores(args.nba)
+    if args.nba:
+        scores.nbaScores(args.d)
     elif args.nbaQ:
-        scores.nbaQuarterScores(args.nbaQ)
+        scores.nbaQuarterScores(args.d)
+    elif args.nhl:
+        scores.nhlScores(args.d)
+    elif args.nfl:
+        print("NFL score feature coming soon")
+    else:
+        parser.print_help()
 
 if __name__ == '__main__':
     main()
